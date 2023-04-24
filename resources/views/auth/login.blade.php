52bd6bde1b00e2644927fa23b4ae38d2ce6a1951
V. Kuleshov <sajkunrnd@gmail.com>
2023-04-24 13:47:03 +0300

    Верстка окна логина

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                     <div class="text-center">
                         <img src="./images/svg/logo-auth.svg" alt="">
                     </div>
-                    <div class="form-auth">
+                    <div class="form-auth mt-2">
                         <div class="form-auth__header"><b>Вход в учётную запись</b></div>
 
                         <div class="form-auth__body mt-3">
@@ -20,7 +20,8 @@
 
                                     <input id="login" type="text"
                                         class="form-control-custom @error('login') is-invalid @enderror" name="login"
-                                        value="{{ old('login') }}" required autocomplete="login" autofocus>
+                                        value="{{ old('login') }}" required autocomplete="login" placeholder="Логин"
+                                        autofocus>
 
                                     @error('login')
                                         <span class="invalid-feedback" role="alert">
@@ -34,7 +35,7 @@
 
                                     <input id="password" type="password"
                                         class="form-control-custom @error('password') is-invalid @enderror" name="password"
-                                        required autocomplete="current-password">
+                                        placeholder="пароль" required autocomplete="current-password">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -45,14 +46,22 @@
 
 
                                 <div class="mt-3">
-                                    <button type="submit" class="btn w-100 btn-custom-reg">
+                                    <button type="submit" class="btn w-100 btn-primary-alt">
                                         {{ __('Войти') }}
                                     </button>
 
-                                    <div class="text-center">
+                                    <div class="mt-4 form-auth__comment">
+                                        <p class="m-0">
+
+                                            Если у вас нет логина или пароля, обратитесь по электронному адресу:
+                                            <a href="mailto:auth@liliani.ru">auth@liliani.ru</a>
+                                        </p>
+                                    </div>
+
+                                    <div class="text-center mt-2">
 
                                         @if (Route::has('password.request'))
-                                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            <a href="{{ route('password.request') }}">
                                                 {{ __('Забыли пароль?') }}
                                             </a>
                                         @endif
