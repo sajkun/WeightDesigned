dfb3d6b105396a6d86c3c8a5f491d95209d5dd81
V. Kuleshov <sajkunrnd@gmail.com>
2023-07-17 17:33:30 +0300

    правка шаблонов авторизации

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul class='p-0'>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
