cc41769dc0570e70854b0133989c89520547729b
V. Kuleshov <sajkunrnd@gmail.com>
2023-08-16 14:09:59 +0300

    Регистрация политик техники

---
+++ app/Policies/PublicArea/VehiclePolicy.php
@@ -0,0 +1,95 @@
+<?php
+namespace App\Policies\PublicArea;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VehiclePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @param  $organisation_id int
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function viewAny(User $user, $organisation_id)
+    {
+        return in_array($user->role, ['user_director', 'user_control_vehicles', 'user_control']) && $user->organisation_id === (int)$organisation_id;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     *  @param  $organisation_id int
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function view(User $user, $organisation_id)
+    {
+        dd('test');
+        return in_array($user->role, ['user_director', 'user_control_vehicles', 'user_control']) && $user->organisation_id === (int)$organisation_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @param  $organisation_id int
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user, $organisation_id)
+    {
+        return in_array($user->role, ['user_control_vehicles', 'user_control']) && $user->organisation_id === (int)$organisation_id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  $organisation_id int
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function update(User $user, $organisation_id)
+    {
+        return in_array($user->role, ['user_control_vehicles', 'user_control']) && $user->organisation_id === (int)$organisation_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  $organisation_id int
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function delete(User $user, $organisation_id)
+    {
+        return in_array($user->role, ['user_control_vehicles', 'user_control']) && $user->organisation_id === (int)$organisation_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  $organisation_id int
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function restore(User $user, $organisation_id)
+    {
+        return in_array($user->role, ['user_control_vehicles', 'user_control']) && $user->organisation_id === (int)$organisation_id;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  $organisation_id int
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function forceDelete(User $user, $organisation_id)
+    {
+        return in_array($user->role, ['user_control_vehicles', 'user_control']) && $user->organisation_id === (int)$organisation_id;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,10 @@ class AuthServiceProvider extends ServiceProvider
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
         'App\Models\User' => 'App\Policies\PublicArea\UserPolicy',
         'App\Models\Employee' => 'App\Policies\PublicArea\EmployeePolicy',
+        'App\Models\Bunker' => 'App\Policies\PublicArea\VehiclePolicy',
+        'App\Models\Tractor' => 'App\Policies\PublicArea\VehiclePolicy',
+        'App\Models\Transporter' => 'App\Policies\PublicArea\VehiclePolicy',
+        'App\Models\Harvester' => 'App\Policies\PublicArea\VehiclePolicy',
     ];
 
     /**
