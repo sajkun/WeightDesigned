6961cb06ea7659ab9331f7e538d303f1281c6697
V. Kuleshov <sajkunrnd@gmail.com>
2023-09-20 15:25:10 +0300

    перевод сообщений об ошибках

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы выслали вам ссылку для сброса пароля!',
+    'throttled' => 'Пожалуйста, подождите прежде чем попробовать повторно.',
+    'token' => 'Токен для сброса пароля не верен.',
+    'user' => 'Пользователь с таким адресом электронной почты не найден.',
 
 ];
