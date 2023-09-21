1833f27b84615318c57eef390c7c21d287387ffc
sajkun <sajkunrnd@gmail.com>
2023-09-21 18:14:44 +0300

    изменение структуры данных о тразакциях весовой системы

--- database/migrations/2023_09_21_151339_create_bvs_data_table.php
+++ database/migrations/2023_09_21_151339_create_bvs_data_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWeightData extends Migration
+class CreateBvsDataTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,21 @@ class CreateWeightData extends Migration
      */
     public function up()
     {
-        Schema::create('weight_data', function (Blueprint $table) {
+        Schema::create('bvs_data', function (Blueprint $table) {
             $table->id();
-            $table->dateTime('operation_time')->nullable();
+            $table->string('bvs_name')->nullable();
+            $table->string('from')->nullable();
+            $table->string('to')->nullable();
+            $table->integer('rfid_status')->nullable();
             $table->integer('ammount_in_bunker')->nullable();
-            $table->integer('ammount_received')->nullable();
-            $table->integer('ammount_given')->nullable();
-            $table->integer('ammount_left')->nullable();
+            $table->integer('ammount_transfered')->nullable();
+            $table->dateTime('operation_time')->nullable();
             $table->string('accelerometer')->nullable();
             $table->boolean('has_check')->nullable();
             $table->string('error')->nullable();
             $table->json('coordinates')->nullable();
             $table->timestamps();
+
         });
     }
 
@@ -35,6 +38,6 @@ class CreateWeightData extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('weight_data');
+        Schema::dropIfExists('bvs_data');
     }
 }
