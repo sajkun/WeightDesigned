6d10e9fd502ca37d68545eb51d18958b62fb000a
V. Kuleshov <sajkunrnd@gmail.com>
2023-10-08 16:37:53 +0300

    изменение заголовка

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Весовая Система - ООО Лилиани') }}</title>
 
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 
